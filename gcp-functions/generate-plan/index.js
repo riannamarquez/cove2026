7b6d2f42d63d8b4cf76d0b9dbb6aea5099f93ef7
Rianna <riannaemarquez@berkeley.edu>
2026-06-07 21:48:22 -0700

    index for generate plan function

---
+++ gcp-functions/generate-plan/index.js
@@ -0,0 +1,67 @@
+ require('dotenv').config({ path: '../../.env'});
+ const { createClient } = require('@supabase/supabase-js');
+ const OpenAI = require('openai')
+ 
+ // loading in each of the packages we installed
+    // dto env tells it to find the env folder and the secret keys in it
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_KEY
+);
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY})
+
+// function gcp will run when someone hits endpoint
+    // req: incoming request
+    // res: response
+// exports.generatePlan tells gcp "this is the function to run"
+exports.generatePlan = async (req, res) => {
+res.set('Access-Control-Allow-Origin', '*');
+if (req.method === 'OPTIONS')
+    { 
+    res.set('Access-Control-Allow-Methods', 'POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type')
+    return res.status(204).send('');
+    }
+
+try {
+    const { body_part, pain_level, condition, goals } = req.body;
+    // pulls out the specific fields from wtv app sent
+    // "pull these four fields out of req.body"
+
+    //rag step-- fetching relevant exercises from your database
+    const { data: exercises, error } = await supabase
+        .from('exercises') // picks the table (of exercise options)
+        .select('*') // grabs all the columns (think sql)
+        .ilike('target_area', `%${body_part}%`) // case-insensitive search
+        .limit(10); // caps at ten results so it doesn't overflow
+
+    const exerciseList = exercises.map(e =>
+        `-${e.name}: ${e.description} (sets: ${e.sets}, reps: ${e.reps})`
+    ).join('\n')
+    // turning the array of exercise objects into a readable text list from suabase
+    // quasiquotes let u embed variables directly with ${} 
+
+    const prompt = `You are a physical therapy assistant...`
+
+    const completion = await openai.chat.completions.create({
+        model: 'gpt-4o',
+        messages: [{ role: 'user', content: prompt }],
+        response_format: { type: 'json_object' }
+    });
+
+    const result = JSON.parse(completion.choices[0].message.content);
+    // sends prompt to OpenAI and waits for the response 
+    // "JSON.parse" makes it so that GPT is forced to return a valid JSON
+        // parses response to an actual javascript object
+
+        return res.status(200).json(result);
+        // sends 3 exercies back to the app 
+
+    // error handling
+} catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Failed to generate plan'});
+}
+};
